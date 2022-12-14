8b8a0ff79ba1a4020da529afcf123cfd3f514e21
ssk11 <ssk29@yandex.ru>
2022-12-14 17:14:22 +0300

    init

---
+++ src/main/java/ru/sskva/phraseadmin/PhraseAdminApplication.java
@@ -0,0 +1,13 @@
+package ru.sskva.phraseadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhraseAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhraseAdminApplication.class, args);
+	}
+
+}
